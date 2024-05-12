bdecb0d5642c9c14a133fae6fdb456497515b606
MaximeVannier27 <maxouvn@gmail.com>
2024-05-12 10:57:43 +0200

    ACID: graphes codecarbon

--- Maxime_ACID/Affichage_pkl.py
+++ Maxime_ACID/Affichage_pkl.py
@@ -2,7 +2,7 @@ import pickle
 
 root = './results/'
 # Charger le fichier .pkl
-with open(root + 'jeu_de_donnees/'+'moyenne_loss.pkl', 'rb') as fichier:
+with open(root + 'jeu_de_donnees/'+'dico_temps.pkl', 'rb') as fichier:
     contenu = pickle.load(fichier)
 
 # Afficher le contenu
