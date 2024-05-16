c5f512f920e097e02bcb8f5f85cc03bd96915a9a
MaximeVannier27 <maxouvn@gmail.com>
2024-05-16 16:51:24 +0200

    cc

--- GRAPHES_COMMUNS.py
+++ GRAPHES_COMMUNS.py
@@ -67,6 +67,7 @@ print("Remy",remy_std_loss[:1000])
 
 
 
+
 plt.figure(figsize=(15, 7.5),facecolor='lightgrey')
 # plt.errorbar(x[:1000], [(maxime_loss[i]) for i in range(len(maxime_loss[:1000]))], yerr=[((maxime_std_loss[i]*0.5)) if (i%70==0) else 0 for i in range(len(maxime_std_loss[:1000]))], label='ACID', color='blue',ecolor='lightblue')
 # plt.errorbar(x[:1000], [(louise_loss[i]) for i in range(len(louise_loss[:1000]))], yerr=[((louise_std_loss[i])) if (i%75==0) else 0 for i in range(len(louise_std_loss[:1000]))], label='DAGMM', color='green',ecolor='lightgreen')
@@ -104,8 +105,8 @@ remy_enviro = 6.60
 
 
 #GRAPHES ENVIRO
-x = ["ACID", "DAGMM", "DNN","N-BAIOT"]
-valeurs_histo = [maxime_enviro,louise_enviro,tanguy_enviro,remy_enviro]
+x = ["DNN", "N-BAIOT", "DAGMM","ACID"]
+valeurs_histo = [tanguy_enviro,remy_enviro,louise_enviro,maxime_enviro]
 
 
 
@@ -127,8 +128,8 @@ louise_precision = 0.6850
 tanguy_precision = 0.9996
 remy_precision = 0.6877
 #METTEZ VOTRE VALEUR DE PRECISION DANS UNE VARIABLE
-x = ["ACID", "DAGMM", "DNN","N-BAIOT"]
-valeurs_histo = [maxime_precision,louise_precision,tanguy_precision,remy_precision]
+x = ["DNN", "N-BAIOT", "DAGMM","ACID"]
+valeurs_histo = [tanguy_precision,remy_precision,louise_precision,maxime_precision]
 
 
 plt.figure(figsize=(15, 7.5),facecolor='lightgrey')
