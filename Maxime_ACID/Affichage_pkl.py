686e70194f7d98df9b8f4704ef1adb55a2605105
MaximeVannier27 <maxouvn@gmail.com>
2024-05-16 10:43:10 +0200

    doh

--- Maxime_ACID/Affichage_pkl.py
+++ Maxime_ACID/Affichage_pkl.py
@@ -1,6 +1,6 @@
 import pickle
 
-root = './results_ekip/jeu_de_donnees/conso_CPU.pkl'
+root = './results_ekip/jeu_de_donnees/accuracy.pkl'
 # Charger le fichier .pkl
 with open(root, 'rb') as fichier:
     contenu = pickle.load(fichier)
